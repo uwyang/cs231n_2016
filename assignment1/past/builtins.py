15437f7c2d7d9b38cc76286b83c31e8ef649b240
yang <yang@jbbmobile.com>
2017-06-17 14:27:19 -0400

    first commit of assignment1, partially complete

---
+++ assignment1/past/builtins.py
@@ -0,0 +1,18 @@
+def xrange(arr):
+    return arr.shape[0]
+
+def load_CIFAR10(ROOT):
+  """ load all of cifar """
+  xs = []
+  ys = []
+  for b in range(1,6):
+    f = os.path.join(ROOT, 'data_batch_%d' % (b, ))
+    X, Y = load_CIFAR_batch(f)
+    xs.append(X)
+    ys.append(Y)    
+  Xtr = np.concatenate(xs)
+  Ytr = np.concatenate(ys)
+  del X, Y
+  Xte, Yte = load_CIFAR_batch(os.path.join(ROOT, 'test_batch'))
+  return Xtr, Ytr, Xte, Yte
+
